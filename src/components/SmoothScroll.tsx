cf9e23beb66cbbb71071cdb599f6bd2587c88007
Yashvanth S <yashvanthsankar@gmail.com>
2026-01-11 22:03:25 +0530

    chore:reduce duration of lenis

--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -6,11 +6,13 @@ import Lenis from '@studio-freight/lenis';
 export default function SmoothScroll({ children }: { children: React.ReactNode }) {
     useEffect(() => {
         const lenis = new Lenis({
-            duration: 0.8,
+            duration: 0.5,
             easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
             orientation: 'vertical',
             gestureOrientation: 'vertical',
             smoothWheel: true,
+            wheelMultiplier: 1,
+            touchMultiplier: 2,
         });
 
         function raf(time: number) {
